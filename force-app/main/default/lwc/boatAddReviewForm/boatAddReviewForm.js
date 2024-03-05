efea14c0ec2c09b629fa6da5838ae5d18eaec966
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2024-03-04 20:57:55 -0500

    fix get set bug in the add review form , create boat review record func

--- force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
+++ force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
@@ -3,6 +3,7 @@ import { LightningElement , api } from "lwc";
 import BOAT_REVIEW_OBJECT from '@salesforce/schema/BoatReview__c';
 import NAME_FIELD from '@salesforce/schema/BoatReview__c.Name';
 import COMMENT_FIELD from '@salesforce/schema/BoatReview__c.Comment__c';
+import { ShowToastEvent } from "lightning/platformShowToastEvent"; 
 
 const SUCCESS_TITLE='Review Created!'; 
 const SUCCESS_VARIANT='success'; 
@@ -19,15 +20,17 @@ export default class BoatAddReviewForm extends LightningElement {
   labelRating  = 'Rating';
   
   // Public Getter and Setter to allow for logic to run on recordId change
+  @api 
   get recordId() { 
-    console.log('from boat add review form: getter' , this.boatId);
     return this.boatId;
   }
   set recordId(value) {
+    // this is a child component . use get and set to modify the value received from the parent 
     //sets boatId attribute
     //sets boatId assignment
-    console.log('from boat add review form: setter' , value);
-    this.boatId = value.recordId;
+    this.setAttribute('boatId', value);
+    this.boatId = value;
+    console.log('this boat: ', this.boatId);
   }
   
   // Gets user rating input from stars component
@@ -39,14 +42,25 @@ export default class BoatAddReviewForm extends LightningElement {
   // This function must prevent the anchor element from navigating to a URL.
   // form to be submitted: lightning-record-edit-form
   handleSubmit(event) {
-    console.log('trying to click submit');
+    event.preventDefault(); // stop the form from submitting
+    const fields = event.detail.fields;
+    fields.Boat__c = this.boatId; 
+    fields.Rating__c = this.rating; 
+    this.template.querySelector('lightning-record-edit-form').submit(fields);
   }
   
   // Shows a toast message once form is submitted successfully
   // Dispatches event when a review is created
-  handleSuccess() {
+  handleSuccess(event) {
     // TODO: dispatch the custom event and show the success message
-    this.handleReset();
+    const toastMessage = new ShowToastEvent({
+      title: SUCCESS_TITLE, 
+      variant: SUCCESS_VARIANT,
+      message:  event.message 
+    })
+    this.dispatchEvent(toastMessage);
+    // triggers a custom event called createreview
+    //this.handleReset();
   }
   
   // Clears form data upon submission
