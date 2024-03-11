42cac4904f9fd26e83303e6e08741eb98e635b2f
YBYuan007 <85644458+YBYuan007@users.noreply.github.com>
2024-03-10 21:38:43 -0400

    similar boats

--- force-app/main/default/lwc/similarBoats/similarBoats.js
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -1,36 +1,46 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import getSimilarBoats from '@salesforce/apex/BoatDataService.getSimilarBoats';
+import { NavigationMixin } from 'lightning/navigation';
 
 // imports
 // import getSimilarBoats
-export default class SimilarBoats extends LightningElement {
+export default class SimilarBoats extends NavigationMixin(LightningElement) {
   // Private
   relatedBoats;
   boatId;
   error;
   
   // public
+  @api
   get recordId() {
       // returns the boatId
+      return this.boatId;
     }
     set recordId(value) {
         // sets the boatId value
+        console.log('really getting the value??' , value);
         // sets the boatId attribute
+        this.boatId = value;
     }
   
   // public
+  @api
   similarBy;
   
   // Wire custom Apex call, using the import named getSimilarBoats
   // Populates the relatedBoats list
-  @wire(getSimilarBoats, {boatId : this.boatId, similarBy : this.similarBy})
+  @wire(getSimilarBoats, {boatId : '$boatId', similarBy : '$similarBy'})
   similarBoats({ error, data }) {
     if(data){
-      console.log('got from backend-data' ,data);
+      console.log('got from backend-data data' ,data);
+      this.relatedBoats = data; 
+      this.error =undefined;
     } else {
-      console.log('got from backend-data' ,error);
+      console.log('got from backend-data error' ,error);
+      this.error = error;
     } 
    }
+
   get getTitle() {
     return 'Similar boats by ' + this.similarBy;
   }
@@ -39,5 +49,15 @@ export default class SimilarBoats extends LightningElement {
   }
   
   // Navigate to record page
-  openBoatDetailPage(event) { }
+  openBoatDetailPage(event) {
+    console.log('redirect: ', event.detail);
+    this[NavigationMixin.Navigate]({
+      type: 'standard__recordPage',
+      attributes: {
+          recordId: event.detail,
+          objectApiName: 'Boat__c',
+          actionName: 'view'
+      },
+  });
+  }
 }
